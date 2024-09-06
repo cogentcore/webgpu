4c0db813e2ef322f6bc3bb5417f931870acdb1d8
Kai O'Reilly <koreilly5297@gmail.com>
2024-09-06 07:59:33 -0700

    need to use Uint8ClampedArray in BytesToJS so that it works in core for making ImageData

--- wgpu/adapt_js.go
+++ wgpu/adapt_js.go
@@ -37,12 +37,12 @@ func ensureBufferSize(size int) {
 	uint8Array = js.Global().Get("Uint8Array").New(arrayBuffer)
 }
 
-// BytesToJS converts the given bytes to a js Uint8Array
+// BytesToJS converts the given bytes to a js Uint8ClampedArray
 // by using the global wasm memory bytes. This avoids the
 // copying present in [js.CopyBytesToJS].
 func BytesToJS(b []byte) js.Value {
 	ptr := uintptr(unsafe.Pointer(&b[0]))
-	memoryBytes := js.Global().Get("Uint8Array").New(js.Global().Get("wasm").Get("instance").Get("exports").Get("mem").Get("buffer"))
+	memoryBytes := js.Global().Get("Uint8ClampedArray").New(js.Global().Get("wasm").Get("instance").Get("exports").Get("mem").Get("buffer"))
 	// using subarray instead of slice gives a 5x performance improvement due to no copying
 	return memoryBytes.Call("subarray", ptr, ptr+uintptr(len(b)))
 }
