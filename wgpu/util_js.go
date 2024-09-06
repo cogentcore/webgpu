e35089e9a725d316b75c321eb4040c66c88c5189
Kai O'Reilly <koreilly5297@gmail.com>
2024-09-06 08:46:09 -0700

    move CopyBytesToJS to util_js.go

--- wgpu/util_js.go
+++ wgpu/util_js.go
@@ -2,7 +2,20 @@
 
 package wgpu
 
-import "syscall/js"
+import (
+	"syscall/js"
+	"unsafe"
+)
+
+// BytesToJS converts the given bytes to a js Uint8ClampedArray
+// by using the global wasm memory bytes. This avoids the
+// copying present in [js.CopyBytesToJS].
+func BytesToJS(b []byte) js.Value {
+	ptr := uintptr(unsafe.Pointer(&b[0]))
+	memoryBytes := js.Global().Get("Uint8ClampedArray").New(js.Global().Get("wasm").Get("instance").Get("exports").Get("mem").Get("buffer"))
+	// using subarray instead of slice gives a 5x performance improvement due to no copying
+	return memoryBytes.Call("subarray", ptr, ptr+uintptr(len(b)))
+}
 
 // mapSlice can be used to transform one slice into another by providing a
 // function to do the mapping.
