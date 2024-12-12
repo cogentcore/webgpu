ad7475f3b4dde476d971b4aca10a12c160dd8dfb
Kai O'Reilly <koreilly5297@gmail.com>
2024-12-11 16:48:32 -0800

    directly pass the offset and length to the Uint8ClampedArray constructor in wgpu.BytesToJS to improve performance and safety; should fix the Cannot perform Construct on a detached ArrayBuffer issue

--- wgpu/util_js.go
+++ wgpu/util_js.go
@@ -12,9 +12,9 @@ import (
 // copying present in [js.CopyBytesToJS].
 func BytesToJS(b []byte) js.Value {
 	ptr := uintptr(unsafe.Pointer(&b[0]))
-	memoryBytes := js.Global().Get("Uint8ClampedArray").New(js.Global().Get("wasm").Get("instance").Get("exports").Get("mem").Get("buffer"))
-	// using subarray instead of slice gives a 5x performance improvement due to no copying
-	return memoryBytes.Call("subarray", ptr, ptr+uintptr(len(b)))
+	// We directly pass the offset and length to the constructor to avoid calling subarray or slice,
+	// thereby improving performance and safety (this fixes a detached array buffer crash).
+	return js.Global().Get("Uint8ClampedArray").New(js.Global().Get("wasm").Get("instance").Get("exports").Get("mem").Get("buffer"), ptr, len(b))
 }
 
 // mapSlice can be used to transform one slice into another by providing a
